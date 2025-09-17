de0dcfac584bada6446bfef2079478f55c7590e2
Frio Alimentaria SAS <app.frioalimentaria@gmail.com>
2025-09-17 21:59:42 +0000

    Try fixing this error: `Runtime Error: Error: selected.some is not a fun

--- src/app/operaciones-manuales-clientes/manual-operations-client-component.tsx
+++ src/app/operaciones-manuales-clientes/manual-operations-client-component.tsx
@@ -1,4 +1,5 @@
 
+
 "use client";
 
 import React, { useState, useMemo, useEffect, useCallback } from 'react';
@@ -599,7 +600,7 @@ export default function ManualOperationsClientComponent({ clients, billingConcep
                                         />
 
                                         {isFixedMonthlyService ? (
-                                            <FormField control={form.control} name="operationDate" render={({ field }) => ( <FormItem className="flex flex-col"><FormLabel>Fecha de Liquidación <span className="text-destructive">*</span></FormLabel><Popover><PopoverTrigger asChild><FormControl><Button variant={"outline"} disabled={dialogMode === 'view'} className={cn("w-full justify-start text-left font-normal", !field.value && "text-muted-foreground")}><CalendarIcon className="mr-2 h-4 w-4" />{field.value ? format(field.value, "PPP", { locale: es }) : <span>Seleccione fecha</span>}</Button></FormControl></PopoverTrigger><PopoverContent className="w-auto p-0" align="start"><Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={dialogMode === 'view'} initialFocus /></PopoverContent></Popover>
+                                            <FormField control={form.control} name="operationDate" render={({ field }) => ( <FormItem className="flex flex-col"><FormLabel>Fecha de Liquidación <span className="text-destructive">*</span></FormLabel><Popover><PopoverTrigger asChild><FormControl><Button variant={"outline"} disabled={dialogMode === 'view'} className={cn("w-full pl-3 text-left font-normal", !field.value && "text-muted-foreground")}><CalendarIcon className="mr-2 h-4 w-4" />{field.value ? format(field.value, "PPP", { locale: es }) : <span>Seleccione fecha</span>}</Button></FormControl></PopoverTrigger><PopoverContent className="w-auto p-0" align="start"><Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={dialogMode === 'view'} initialFocus /></PopoverContent></Popover>
                                                 {field.value && <FormDescription>Se liquidarán {getDaysInMonth(field.value)} días para el mes de {format(field.value, 'MMMM', {locale: es})}.</FormDescription>}
                                                 <FormMessage /></FormItem> )} />
                                         ) : isBulkMode ? (
@@ -632,6 +633,8 @@ export default function ManualOperationsClientComponent({ clients, billingConcep
                                                             selected={field.value || []}
                                                             onSelect={field.onChange}
                                                             disabled={(date) => isSunday(date) || dialogMode === 'view'}
+                                                            initialFocus
+                                                            month={field.value?.[0]}
                                                             />
                                                         </PopoverContent>
                                                     </Popover>
@@ -952,5 +955,3 @@ const ExcedentManager = () => {
         </div>
     )
 }
-
-    
