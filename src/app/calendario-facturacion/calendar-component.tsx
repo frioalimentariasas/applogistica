c185b90142ab06bef2cd1f845c530d9d0febd68f
Frio Alimentaria SAS <app.frioalimentaria@gmail.com>
2026-01-08 15:47:09 +0000

    Try fixing this error: `Console Error: React does not recognize the `dis

--- src/app/calendario-facturacion/calendar-component.tsx
+++ src/app/calendario-facturacion/calendar-component.tsx
@@ -140,7 +140,7 @@ export default function CalendarComponent({ clients }: { clients: ClientInfo[] }
     setEventToDelete(null);
   };
   
-  const DayContent = ({ date, ...props }: { date: Date } & any) => {
+  const DayContent = ({ date, displayMonth, ...props }: { date: Date, displayMonth: Date } & any) => {
     const dayEvents = events.filter(e => e.date === format(date, 'yyyy-MM-dd'));
     
     return (
